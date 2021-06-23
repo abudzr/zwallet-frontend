96be7e65df37b262fb037aae0d9729398fdd1e29
Abu Dzar Al-Ghifari <68935056+abudzr@users.noreply.github.com>
2021-06-23 11:29:04 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-        api: 'https://be-zwallet.abudzaralghifari.xyz/api/v1',
-        api_img: 'https://be-zwallet.abudzaralghifari.xyz/',
+        api: 'https://zwallet.project-be.xyz/api/v1',
+        api_img: 'https://zwallet.project-be.xyz/',
     },
 }
